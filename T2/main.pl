54010e9263d606e635068c32484411b016876820
MeBeeDaisy <up201505505@fe.up.pt>
2017-12-13 17:58:07 +0000

    it is working

--- T2/main.pl
+++ T2/main.pl
@@ -1,4 +1,5 @@
 :-use_module(library(clpfd)).
+:-use_module(library(lists)).
 
 :-include('puzzles.pl').
 
@@ -20,26 +21,46 @@ printPuzzle([H|T]) :-
 	write(H),
 	printPuzzle(T).
 
+paintWay(List, Num, Pos):-
+	length(List, L),
+	Rest is L - Num,
+	global_cardinality(List, [1-Num, 0-Rest]),
+	element(Pos, List, 0).
 
-paintWay(List, Num):-
-	global_cardinality(List, [1-Num]),
-	element(4, List, 1).
+getSubList(Pos, PuzzleF, [Head], N, Aux):-
+	Aux == 9,!,
+	nth1(Pos, PuzzleF, Head).
+
+getSubList(Pos, PuzzleF, [Head | Tail], N, Aux):-
+	(Aux == 3 ; Aux == 6),!,
+	nth1(Pos, PuzzleF, Head),
+	NewPos is Pos + N - 2,
+	Aux1 is Aux + 1,
+	getSubList(NewPos, PuzzleF, Tail, N, Aux1).
+
+getSubList(Pos, PuzzleF, [Head|Tail], N, Aux):-
+	nth1(Pos, PuzzleF, Head),
+	Aux1 is Aux + 1,
+	NewPos is  Pos + 1,
+	getSubList(NewPos, PuzzleF, Tail, N, Aux1).
 
 
-cycle([], []).
-cycle([H | T], PuzzleF):-
-	H \== nil.
-	/*paintWay(PuzzleF, H).*/
-cycle([H | T], [HF | TF]):-
-	HF in 0..1,
-	cycle(T, TF).
+cycle([], _, _).
+cycle([Num-Pos | T], PuzzleF, N):-
+	LeftTopPos is Pos - N - 1,
+	write('HALO'),nl,
+	getSubList(LeftTopPos, PuzzleF, List, N, 1),
+	write(List),nl,
+	write(PuzzleF),nl,
+	paintWay(List, Num, 5),
+	cycle(T, PuzzleF, N).
 
-teste :- puzzle6(Puzzle), printPuzzle(Puzzle).
-
-teste1 :- 
-	puzzle3(Puzzle),
-	length(PuzzleF, 9),
+teste(PuzzleF):-
+	puzzle4(Puzzle),
+	numbers(Numbers),
+	length(PuzzleF, 16),
+	N is 4, %side of matrix
 	domain(PuzzleF, 0, 1),
-	cycle(Puzzle, PuzzleF),
+	cycle(Numbers, PuzzleF, N),
 	labeling([], PuzzleF),
-	printPuzzle(PuzzleF).
+	write(PuzzleF).
--- T2/puzzles.pl
+++ T2/puzzles.pl
@@ -9,4 +9,6 @@ puzzle6_linha([nil, nil, nil, nil, nil, nil, nil, 5, nil, nil, 5, nil, nil, nil,
 				nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, 3, nil, nil, nil, nil,
 				nil, nil, nil, nil, nil, 2]).
 
-puzzle3([nil, nil, nil, nil, 8, nil, nil, nil, nil]).
+puzzle3([nil, nil, nil, nil, 5, nil, nil, nil, nil]).
+puzzle4([nil, nil, nil, nil, nil, 5, nil, nil, nil, nil, 3, nil, nil, nil, nil, nil]).
+numbers([5-6, 3-11]). %num-pos
