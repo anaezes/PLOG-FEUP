0bad1ef1546bb2ff2ebd0f691508a964215e1a92
Ana Santos <anaezes@gmail.com>
2017-12-14 00:50:56 +0000

    first restriction done

--- T2/puzzles.pl
+++ T2/puzzles.pl
@@ -1,14 +1,16 @@
-puzzle6([[nil, nil, nil, nil, nil, nil],
-		[nil, 5, nil, nil, 5, nil],
-		[nil, nil,3, nil, nil, nil],
-		[nil, nil, nil, nil, nil, nil],
-		[nil, 3, nil, nil, nil, nil],
-		[nil, nil, nil, nil, nil, 2]]).
+puzzle6([nil, nil, nil, nil, nil, nil,
+		nil, 5, nil, nil, 5, nil,
+		nil, nil,3, nil, nil, nil,
+		nil, nil, nil, nil, nil, nil,
+		nil, 3, nil, nil, nil, nil,
+		nil, nil, nil, nil, nil, 2]).
+numbers6([5-8, 5-11, 3-15, 3-26, 2-36]). %num-pos
 
 puzzle6_linha([nil, nil, nil, nil, nil, nil, nil, 5, nil, nil, 5, nil, nil, nil,3, 
 				nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, 3, nil, nil, nil, nil,
 				nil, nil, nil, nil, nil, 2]).
 
 puzzle3([nil, nil, nil, nil, 5, nil, nil, nil, nil]).
-puzzle4([nil, nil, nil, nil, nil, 5, nil, nil, nil, nil, 3, nil, nil, nil, nil, nil]).
-numbers([5-6, 3-11]). %num-pos
+
+puzzle4([nil, nil, nil, nil, nil, nil, 5, nil, nil, 3, nil, nil, nil, nil, nil, nil]).
+numbers([5-7, 3-10]). %num-pos
